ac2da5ff35bfea24ca3b4f1254b686a47368ff22
mwl <itexplorer@126.com>
2018-02-19 00:24:44 +0800

    修改~~~

--- pages/contactUs/contactUs.js
+++ pages/contactUs/contactUs.js
@@ -1,17 +1,33 @@
 const kefuUrls = "http://app.zrqgj.cn/steward_api/managerInfos"
+const issueUrl = "http://app.zrqgj.cn/steward_api/customerInfos"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    navItems:[]
+    navItems:[],
+    userName:'',
+    phone:''
   },
   /**
   * 生命周期函数--监听页面加载
   */
   onLoad: function (options) {
     this.onInitDate();
+  },
+
+  
+  usernameInput:function(e){
+    this.setData({
+      userName:e.detail.value
+    })
+  },
+
+ phoneInput: function (e) {
+    this.setData({
+      phone: e.detail.value
+    })
   },
 
   //拨打电话
@@ -50,6 +66,57 @@ Page({
 
       fail: function ({ errMsg }) {
         wx.hideLoading();
+      }
+    })
+  },
+
+  //提交请求
+  changeBtnClick: function (e) {
+    var self = this
+    if (self.data.userName == null || self.data.userName == '') {
+      wx.showToast({
+        title: '请输入您的名字',
+        icon: "none"
+      })
+      return
+    }
+    if (self.data.phone == null || self.data.phone == '') {
+      wx.showToast({
+        title: '请输入手机号',
+        icon: "none"
+      })
+      return
+    }
+    wx.showLoading({
+      title: '提交中',
+      mask: true
+    })
+    wx.request({
+      url: issueUrl,
+      data: {
+        name: self.data.userName,
+        contact: self.data.phone
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      method: 'POST',
+      success: function (result) {
+        wx.hideLoading();
+        wx.showToast({
+          title: "提交成功",
+          icon: "success"
+        })
+        wx.navigateBack({
+          delta: 1
+        })
+      },
+
+      fail: function ({ errMsg }) {
+        wx.hideLoading();
+        self.setData({
+          loading: false
+        })
       }
     })
   },
